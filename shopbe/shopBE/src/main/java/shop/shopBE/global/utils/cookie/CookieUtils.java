cb67a87227eb064f6d93b4d4afe86f660674633f
이경훈 <100902332+lgh000322@users.noreply.github.com>
2025-01-22 12:38:31 +0900

    feat : 쿠키 정책 변경

--- shopbe/shopBE/src/main/java/shop/shopBE/global/utils/cookie/CookieUtils.java
+++ shopbe/shopBE/src/main/java/shop/shopBE/global/utils/cookie/CookieUtils.java
@@ -8,11 +8,21 @@ public class CookieUtils {
 
     public <T extends CookieData> Cookie createCookie(T cookieData) {
         Cookie cookie = new Cookie(cookieData.getKey(), cookieData.getValue());
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false);
+
+        if (cookieData.getKey().equals("accessToken")) {
+            // accessToken인 경우에 자바스크립트로 가져올 수 있게 함.
+            cookie.setHttpOnly(false);
+        } else {
+            // accessToken이 아닌 refreshToken일 경우에 자바스크립트로 가져갈 수 없게 함.
+            cookie.setHttpOnly(true);
+        }
+
+        cookie.setSecure(true);
         cookie.setMaxAge(cookie.getMaxAge());
         cookie.setPath("/");
         cookie.setAttribute("SameSite", "None");
         return cookie;
     }
+
+
 }
