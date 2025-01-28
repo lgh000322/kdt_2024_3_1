66bdd99084ba2f8cdd135a0ba2599273d7c2368b
이경훈 <100902332+lgh000322@users.noreply.github.com>
2025-01-28 22:22:11 +0900

    fix : 호환되지 않는 valid 어노테이션 제거

--- shopbe/shopBE/src/main/java/shop/shopBE/domain/likesitem/request/LikesPaging.java
+++ shopbe/shopBE/src/main/java/shop/shopBE/domain/likesitem/request/LikesPaging.java
@@ -6,11 +6,9 @@ import jakarta.validation.constraints.NotEmpty;
 
 public record LikesPaging(
         @Min(value = 1)
-        @NotEmpty
         int page,
 
         @Max(value = 10)
-        @NotEmpty
         int size
 ) {
 }
