bfc5296c117431939e9c20f440ddbb41dc038df1
이경훈 <100902332+lgh000322@users.noreply.github.com>
2025-02-06 14:54:45 +0900

    사이드바 수정

--- shop/src/layouts/BasicLayout.js
+++ shop/src/layouts/BasicLayout.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import HeaderComponent from "../components/HeaderComponent";
 import Sidebar from "../components/SideBar";
-import { useSelector } from "react-redux";
 
 function BasicLayout({ children }) {
   const [selectedMenu, setSelectedMenu] = useState(null);
@@ -20,7 +19,7 @@ function BasicLayout({ children }) {
       <div className="flex flex-1">
         {/* 사이드바 */}
         <div className="w-64 bg-gray-50 pt-16 fixed left-0 top-30 h-full shadow-md">
-          <Sidebar role={role} onMenuClick={handleMenuClick} />
+          <Sidebar onMenuClick={handleMenuClick} />
         </div>
 
         {/* 메인 콘텐츠 */}
