3762cbc9dfa1951ab53ec2c435f549ed6ada0d54
dltjdska32 <dltjdska32@naver.com>
2025-02-07 13:59:23 +0900

    fix: Requestpart 수정

--- shopbe/shopBE/src/main/java/shop/shopBE/global/config/SecurityConfig.java
+++ shopbe/shopBE/src/main/java/shop/shopBE/global/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
             "/JWT/**",
             "/member/logout",
             "/banners/**",
-            "/refresh/**"
+            "/refresh/**",
+            "/product",
     };
 
     @Bean
