4aea346c4e93b707d3164059a0a1af914b2d841f
이경훈 <100902332+lgh000322@users.noreply.github.com>
2025-02-12 18:31:56 +0900

    chore : 주소 경로 변경

--- shopbe/shopBE/src/main/java/shop/shopBE/global/config/WebCorsConfig.java
+++ shopbe/shopBE/src/main/java/shop/shopBE/global/config/WebCorsConfig.java
@@ -20,7 +20,7 @@ public class WebCorsConfig implements WebMvcConfigurer {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); // 서로 다른 도메인 간 쿠키 전달 허용
-        config.setAllowedOrigins(List.of("https://api.fmanshop.com", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://api.fmanshop.com", "http://localhost:3000","https://kdt2024-3-1-6qr6njsq1-lgh000322s-projects.vercel.app"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Cache-Control", "Access-Control-Allow-Origin"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 메서드, OPTIONS는 pre-flight 요청을 위해 허용해야 함.
         config.setMaxAge(3600L); // 최대 캐시 시간
